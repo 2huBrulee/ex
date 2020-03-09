3881e2ea6daacdfb99ddc1f51803a689f5c200b1
2hubrulee <alejandro2hpxa@gmail.com>
2020-03-09 16:50:44 -0500

    initial D

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const sendData = data => {
+  axios
+    .post(`localhost:8000/user`, data)
+    .then(result => console.log(result))
+    .catch(err => console.trace(err));
+};
+
+const handleOnSubmit = e => {
+  e.preventDefault();
+  sendData({
+    nombres: e.target.name.value,
+    apellidos: e.target.lastname.value,
+    email: e.target.email.value,
+    telefono: e.target.phone.value
+  });
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleOnSubmit}>
+        <div>
+          <label>Nombres</label>
+          <input type="text" name="name"></input>
+        </div>
+        <div>
+          <label>Apellidos</label>
+          <input type="text" name="lastname"></input>
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="email" name="email"></input>
+        </div>
+        <div>
+          <label>Telefono</label>
+          <input type="tel" maxLength="9" name="phone"></input>
+        </div>
+
+        <div>
+          <input type="submit" name="botonForm" value="ENVIAR" />
+        </div>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
