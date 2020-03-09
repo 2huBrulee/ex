84c390ebcdb185aa5e9802557c71fd83d2ad76e7
2hubrulee <alejandro2hpxa@gmail.com>
2020-03-09 16:53:33 -0500

    numbers on fone

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
         </div>
         <div>
           <label>Telefono</label>
-          <input type="tel" maxLength="9" name="phone"></input>
+          <input type="text" pattern="\d*" maxlength="9" name="phone"></input>
         </div>
 
         <div>
